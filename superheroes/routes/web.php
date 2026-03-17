ed335803457528c5ca0b80befa13e90ebb23b6d4
josemario234 <AL02963295@tecmilenio.mx>
2026-03-17 14:00:40 -0700

    Creating the routes

--- superheroes/routes/web.php
+++ superheroes/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('superheroes', 'SuperheroController');
+Route::get('superheroes', 'SuperheroController@index')->name('superheroes.index');
+Route::post('superheroes', 'SuperheroController@store')->name('superheroes.store');
+Route::post('superheroes/create', 'SuperheroController@create')->name('superheroes.create');
+Route::get('superheroes/{id}', 'SuperheroController@show')->name('superheroes.show');
+Route::get('superheroes/{id}/edit', 'SuperheroController@edit')->name('superheroes.edit');
+Route::patch('superheroes/{id}', 'SuperheroController@update')->name('superheroes.update');
+Route::delete('superheroes/{id}', 'SuperheroController@destroy')->name('superheroes.destroy');
